e1ae1134d6091bfc13ea930d4bc2c5c0e8788291
Szymon Bosiak <szymon.bosiak.99@gmail.com>
2021-12-12 17:10:20 +0100

    created rewards page

--- main.js
+++ main.js
@@ -60,20 +60,6 @@ for (let i = 0; i < track.length; i++) {
                nextBtn.classList.remove('hidden');
            };
        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
 
     check();
@@ -101,6 +87,7 @@ for (let i = 0; i < track.length; i++) {
     });
 
 }
+
 
 /* REACTIVE FOOTER */
 
@@ -151,6 +138,52 @@ const handleSecondClick = () => {
 
 for (let i = 0; i < menu_toggle.length; i++) {
     menu_toggle[i].addEventListener('click', handleSecondClick);
+}
+
+
+/* STARS VALUE */
+
+const selected_star_amount = document.querySelectorAll('.selected_star_amount');
+const active_star_amount = document.querySelector('.selected_star_amount--active');
+const stars_selected_25 = document.getElementById('25');
+const stars_selected_50 = document.getElementById('50');
+const stars_selected_150 = document.getElementById('150');
+const stars_selected_200 = document.getElementById('200');
+const stars_selected_400 = document.getElementById('400');
+
+const content_window = document.querySelector('.content_window');
+const stars_img = document.getElementById('stars_img');
+const stars_h3 = document.getElementById('stars_h3');
+const stars_p = document.getElementById('stars_p');
+
+for (let i = 0; i < selected_star_amount.length; i++) {
+
+    selected_star_amount[i].addEventListener('click', e => {
+        selected_star_amount.forEach(el => el.classList.remove('selected_star_amount--active'));
+        selected_star_amount[i].classList.toggle('selected_star_amount--active');
+
+            if (selected_star_amount[i] === selected_star_amount[0]) {
+                stars_img.src = 'img/Rewards/025.png';
+                stars_h3.innerHTML = 'Customize your drink';
+                stars_p.innerHTML = 'Make your drink just right with an extra espresso shot, dairy substitute or a dash of your favorite syrup.';
+            } else if (selected_star_amount[i] === selected_star_amount[1]) {
+                stars_img.src = 'img/Rewards/050.png';
+                stars_h3.innerHTML = 'Brewed hot coffee, bakery item or hot tea';
+                stars_p.innerHTML = 'Pair coffee cake or an almond croissant with your fresh cup of hot brew.';
+            } else if (selected_star_amount[i] === selected_star_amount[2]) {
+                stars_img.src = 'img/Rewards/150.png';
+                stars_h3.innerHTML = 'Handcrafted drink, hot breakfast or parfait';
+                stars_p.innerHTML = 'Have a really good morning with a breakfast sandwich, oatmeal or your favorite drink.';
+            } else if (selected_star_amount[i] === selected_star_amount[3]) {
+                stars_img.src = 'img/Rewards/200.png';
+                stars_h3.innerHTML = 'Salad, sandwich or protein box';
+                stars_p.innerHTML = 'Nourish your day with a hearty Chipotle Chicken Wrap or Eggs & Cheese Protein Box.';
+            } else if (selected_star_amount[i] === selected_star_amount[4]) {
+                stars_img.src = 'img/Rewards/400.png';
+                stars_h3.innerHTML = 'Select merchandise or at-home coffee';
+                stars_p.innerHTML = 'Take home a signature cup, a bag of coffee or your choice of select coffee accessories.';
+            }
+    });
 }
 
 
